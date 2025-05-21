63e40cb4994980066f81769447021bfa025d34c3
marco9813 <106922930+marco9813@users.noreply.github.com>
2025-05-21 18:20:46 +0000

    Batalha Naval utilizando loope array matriz, vetor

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    int tabuleiro[10][10];
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    // Aqui eu criei uma matriz de batalha naval só com 0s
+    for (int j = 0; j < 10; j++){
+        for(int k = 0; k < 10; k++){
+            tabuleiro[j][k] = 0;
+        }
+    }
+
+    // Aqui eu defini as posições dos navios na horizontal
+    for(int h = 2; h < 5; h++){
+        tabuleiro[2][h + 1] = 3;
+    }
+
+    // Aqui eu defini as posições dos navios na Vertical
+    for(int v = 4; v < 7; v++){
+        tabuleiro[v + 1][1] = 3;
+    }
+
+    printf("Jogo Batalha Naval \n");
+    printf("  ");
+
+    // Aqui eu criei um alfabeto que vai de A até J
+    for (int i = 0; i < 10; i++){
+        printf("%c ", linha[i]);
+    }
+
+    for(int j = 0; j < 10; j++){
+        printf(" \n"); // Aqui ele vai pular a linha para não colidir com o alfabeto
+        printf("%d", j + 1); // Aqui é a numeração que fica no lado esquerdo da matriz que vai de 1 até 10
+        for(int k = 0; k < 10; k++){
+            printf(" %d", tabuleiro[j][k]); // Aqui imprimi todo tabuleiro da batalha naval com os navios definido
+   }
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    }
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+    
     return 0;
 }
